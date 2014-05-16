bf20c38fe7d202ea1c5347b8fb3f35d7988ad4c4
efraim <efraim.bayarri@gmail.com>
2014-05-16 14:29:40 +0200

    Versió: 2014.20.5
    Change-Id: Icdc818c88e416709956b43a2128caeaed65164b2
    Signed-off-by: efraim <efraim.bayarri@gmail.com>

---
+++ ric-alu.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: RIC-ALU
+ * Plugin URI: http://replicantsfactory.com/
+ * Author: Efraim Bayarri
+ * Author URI: http://replicantsfactory.com/
+ * Version: 2014.20.5
+ * Description: Projecte Web Alumnes (Escola Ramon i Cajal)
+ * Release Version:(??)
+ * Release Date: ??
+ */
+
+##	BEGIN debug
+require_once(WP_PLUGIN_DIR.'/ricca3/dumper.php');
+require_once(WP_PLUGIN_DIR.'/ricca3/dump_r.php');
+##	END debug
